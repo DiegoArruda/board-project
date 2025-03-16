884917de9d35f5f550500d9b663ca57672750af3
Diego <arrudadiego1@gmail.com>
2025-03-16 14:31:09 -0300

    settings envs

--- src/main/java/br/com/avanade/persistence/config/ConnectionConfig.java
+++ src/main/java/br/com/avanade/persistence/config/ConnectionConfig.java
@@ -13,8 +13,8 @@ public final class ConnectionConfig {
 
     public static Connection getConnection() throws SQLException {
         var url = "jdbc:mysql://localhost/board";
-        var user = "root";
-        var password = "5shyrbdi";
+        var user = System.getenv("DB_USER");
+        var password = System.getenv("DB_PASSWORD");
         var connection = DriverManager.getConnection(url, user, password);
         connection.setAutoCommit(false);
         return connection;
